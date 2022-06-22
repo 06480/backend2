7742397f931cf3455f58321c70dd60c51ff27908
06480 <paul.messiant@gmail.com>
2022-06-22 14:51:04 +0200

    modifs

--- routes/sauce.js
+++ routes/sauce.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/sauce')
+const multer = require('../middleware/multer-config')
 
-router.post('/', stuffCtrl.createSauce);
-router.put('/:id', stuffCtrl.modifySauce);
+router.post('/', multer, stuffCtrl.createSauce);
+router.put('/:id', multer, stuffCtrl.modifySauce);
 router.delete('/:id', stuffCtrl.deleteSauce);
 router.get('/:id', stuffCtrl.getOneSauce);
 router.get('/', stuffCtrl.getAllSauces);
