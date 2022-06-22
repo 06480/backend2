96d2cbae4672d52116e419b9211ae52683ecbbe9
06480 <paul.messiant@gmail.com>
2022-06-22 22:43:19 +0200

    modifs

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -2,14 +2,16 @@ const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.thing);
+    console.log(req.body.sauce)
+  const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
     const sauce = new Sauce({
       ...sauceObject,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-
     });
+    
     sauce.save()
+    
       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
       .catch(error => res.status(400).json({ error }));
 };
